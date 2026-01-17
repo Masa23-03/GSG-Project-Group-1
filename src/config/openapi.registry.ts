22e3614a8975baf2709bdb08d126eaf4dce81d89
Masa Sabobeh <masasabobeh03@gmail.com>
2026-01-17 06:25:09 +0200

    feat: remove OpenAPI registry configuration file

--- src/config/openapi.registry.ts
+++
@@ -1,5 +0,0 @@
-import { OpenAPIRegistry } from '@asteasolutions/zod-to-openapi';
-import { z } from 'zod';
-import '@asteasolutions/zod-to-openapi/extendZodWithOpenApi';
-
-export const registry = new OpenAPIRegistry();
