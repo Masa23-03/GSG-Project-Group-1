51569172b9c2bcb0b672fda223736676a659565c
Masa Sabobeh <masasabobeh03@gmail.com>
2026-01-17 04:42:53 +0200

    feat: enable CORS with credentials support in main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { env } from './config/env';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
 
   await app.listen(env.PORT);
 }
