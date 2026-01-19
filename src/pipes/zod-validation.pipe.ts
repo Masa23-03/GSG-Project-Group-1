859dab734cb327b6ec39432022e75bdf833c15a7
Masa Sabobeh <masasabobeh03@gmail.com>
2026-01-19 04:40:29 +0200

    feat: add zod validation pipe

---
+++ src/pipes/zod-validation.pipe.ts
@@ -0,0 +1,11 @@
+import { ArgumentMetadata, PipeTransform } from '@nestjs/common';
+import { ZodType } from 'zod';
+
+export class ZodValidationPipe<T> implements PipeTransform<T, T> {
+  constructor(private schema: ZodType<T>) {}
+  transform(value: T, metadata: ArgumentMetadata): T {
+    const parsedValue = this.schema.parse(value);
+    console.log('Value after parsing: ' + parsedValue);
+    return parsedValue;
+  }
+}
