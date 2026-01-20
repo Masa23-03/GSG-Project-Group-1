56de7407f578bf830846c94dd9a4c64ae03f5280
Masa Sabobeh <masasabobeh03@gmail.com>
2026-01-20 11:47:35 +0200

    feat: add driver and pharmacy modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
+import { DriverModule } from './modules/driver/driver.module';
+import { PharmacyModule } from './modules/pharmacy/pharmacy.module';
 
 @Module({
   imports: [
@@ -10,6 +12,8 @@ import { UserModule } from './modules/user/user.module';
     }),
     AuthModule,
     UserModule,
+    DriverModule,
+    PharmacyModule,
   ],
 })
 export class AppModule {}
