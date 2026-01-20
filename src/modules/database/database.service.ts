2603c7c14e92bcb1594bbeca97f2a596bd710c6a
Masa Sabobeh <masasabobeh03@gmail.com>
2026-01-20 13:15:40 +0200

    feat: implement database module

--- src/modules/database/database.service.ts
+++ src/modules/database/database.service.ts
@@ -1,7 +1,43 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { env } from 'src/config/env';
+import {
+  PaginationQueryType,
+  PaginationResponseType,
+} from 'src/types/unifiedType.types';
 
 @Injectable()
-export class DatabaseService {
- 
+export class DatabaseService
+  extends PrismaClient
+  implements OnModuleDestroy, OnModuleInit
+{
+  constructor() {
+    super();
+  }
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+  async onModuleInit() {
+    await this.$connect();
+  }
+  //handle query pagination
+  handleQueryPagination(query: PaginationQueryType) {
+    const page = Number(query.page ?? 1);
+    const limit = Number(query.limit ?? 10);
+
+    return { skip: (page - 1) * limit, take: limit, page };
+  }
+  //format pagination response
+  formatPaginationResponse(args: {
+    count: number;
+    limit: number;
+    page: number;
+  }): PaginationResponseType {
+    return {
+      total: args.count,
+      limit: args.limit,
+      page: args.page,
+      totalPages: Math.ceil(args.count / args.limit),
+    };
+  }
 }
