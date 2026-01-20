8dd34031ea1f64e4aa916702eb18d2ee2f7473cd
Masa Sabobeh <masasabobeh03@gmail.com>
2026-01-20 15:57:23 +0200

    fix: correctly configure JwtModule registration

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { env } from 'src/config/env';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
+  imports: [
+    JwtModule.registerAsync({
+      global: true,
+      useFactory: async () => ({
+        secret: env.JWT_SECRET,
+        signOptions: { expiresIn: '15m' },
+      }),
+    }),
+  ],
 })
 export class AuthModule {}
