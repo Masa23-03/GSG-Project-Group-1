bcc7a314c7d64fab827225d8df14109aab562e9f
Masa Sabobeh <masasabobeh03@gmail.com>
2026-01-22 20:43:04 +0200

    feat: add file module

--- src/modules/file/file.module.ts
+++ src/modules/file/file.module.ts
@@ -1,8 +1,26 @@
 import { Global, Module } from '@nestjs/common';
 import { FileService } from './file.service';
-
+import { MulterModule } from '@nestjs/platform-express';
+import { ImageKitProvider } from './imagekit.provider';
+import { FileController } from './file.controller';
+import multer from 'multer';
 @Global()
 @Module({
-  providers: [FileService],
+  imports: [
+    MulterModule.register({
+      storage: multer.diskStorage({
+        destination: './tmp/uploads',
+
+        filename: (req, file, cb) => {
+          cb(null, `${Date.now()}-${file.originalname}`);
+        },
+      }),
+
+      limits: { fileSize: 15 * 1024 * 1024 },
+    }),
+  ],
+  controllers: [FileController],
+  providers: [FileService, ImageKitProvider],
+  exports: [FileService],
 })
 export class FileModule {}
