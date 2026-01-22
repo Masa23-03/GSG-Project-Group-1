c25c357bc0e9ad4d6c1286c5593c76015c38df4b
Masa Sabobeh <masasabobeh03@gmail.com>
2026-01-22 17:42:06 +0200

    feat: add imagekit provider

--- src/modules/file/imagekit.provider.ts
+++ src/modules/file/imagekit.provider.ts
@@ -1,11 +1,17 @@
 // import ImageKit from '@imagekit/nodejs';
 
-// export const imageKitProvider = {
-//   provider: ImageKit,
-//   useFactory: () => {
-//     return new ImageKit({
-//         privateKey:
+import ImageKit from '@imagekit/nodejs';
+import { ConfigService } from '@nestjs/config';
+import { env } from 'src/config/env';
 
-//     });
-//   },
-// };
+export const IMAGEKIT = Symbol('IMAGEKIT');
+
+export const imageKitProvider = {
+  provide: IMAGEKIT,
+
+  useFactory: () => {
+    return new ImageKit({
+      privateKey: env.IMAGEKIT_PRIVATE_KEY,
+    });
+  },
+};
