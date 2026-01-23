5fed00878b2892ca64e37e3c021386063d778835
Masa Sabobeh <masasabobeh03@gmail.com>
2026-01-23 20:58:38 +0200

    feat: add logging interceptor

--- src/interceptors/logging.interceptor.ts
+++ src/interceptors/logging.interceptor.ts
@@ -4,14 +4,29 @@ import {
   Injectable,
   NestInterceptor,
 } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
+import { Request, Response } from 'express';
 
 @Injectable()
-export class loggingInterceptor implements NestInterceptor {
+export class LoggingInterceptor implements NestInterceptor {
   intercept(
     context: ExecutionContext,
     next: CallHandler<any>,
   ): Observable<any> | Promise<Observable<any>> {
-    throw new Error('Method not implemented.');
+    const date = Date.now();
+    const http = context.switchToHttp();
+    const req = http.getRequest<Request>();
+    const res = http.getResponse<Response>();
+
+    console.log(`Before... ${req.method} ${req.url}`);
+
+    return next.handle().pipe(
+      tap(() => {
+        const duration = Date.now() - date;
+        console.log(
+          `After...  ${req.method} ${req.url}  -status code: ${res.status} - ${duration}ms`,
+        );
+      }),
+    );
   }
 }
