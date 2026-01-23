5a43f625967b8d0cc3b2ca75f3968c09e3394a5d
Masa Sabobeh <masasabobeh03@gmail.com>
2026-01-23 15:30:44 +0200

    fix: its commented - not important now

--- src/filters/global-exception.filter.ts
+++ src/filters/global-exception.filter.ts
@@ -1,18 +1,23 @@
 import {
   ArgumentsHost,
   Catch,
-  ExceptionFilter,
+  HttpException,
   HttpStatus,
 } from '@nestjs/common';
+import { BaseExceptionFilter } from '@nestjs/core';
 import { Request, Response } from 'express';
 import { ApiErrorResponse } from 'src/types/unifiedType.types';
 
 @Catch()
-export class UncaughtException implements ExceptionFilter {
+export class UncaughtException extends BaseExceptionFilter {
   catch(exception: unknown, host: ArgumentsHost) {
+    if (exception instanceof HttpException) {
+      return super.catch(exception, host);
+    }
     const ctx = host.switchToHttp();
     const res = ctx.getResponse<Response>();
     const req = ctx.getRequest<Request>();
+    console.error('[UnknownError]', exception);
     const errorResponse: ApiErrorResponse = {
       success: false,
       message: 'Internal server error',
