57b97c7e01eb5383e99c70262358b7ae42e0b708
Masa Sabobeh <masasabobeh03@gmail.com>
2026-01-23 12:27:09 +0200

    feat: add cloudinary to file module

--- src/modules/file/file.module.ts
+++ src/modules/file/file.module.ts
@@ -1,11 +1,11 @@
 import { Global, Module } from '@nestjs/common';
 import { FileService } from './file.service';
 import { MulterModule } from '@nestjs/platform-express';
-import { ImageKitProvider } from './imagekit.provider';
 import { FileController } from './file.controller';
 import multer from 'multer';
 import * as path from 'node:path';
 import * as fs from 'node:fs';
+import { CloudinaryProvider } from './cloudinary.provider';
 const uploadDir = path.resolve(process.cwd(), 'tmp', 'uploads');
 fs.mkdirSync(uploadDir, { recursive: true });
 
@@ -15,17 +15,15 @@ fs.mkdirSync(uploadDir, { recursive: true });
     MulterModule.register({
       storage: multer.diskStorage({
         destination: uploadDir,
-
         filename: (req, file, cb) => {
           cb(null, `${Date.now()}-${file.originalname}`);
         },
       }),
-
       limits: { fileSize: 15 * 1024 * 1024 },
     }),
   ],
   controllers: [FileController],
-  providers: [FileService, ImageKitProvider],
+  providers: [FileService, CloudinaryProvider],
   exports: [FileService, MulterModule],
 })
 export class FileModule {}
