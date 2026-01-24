7de1ddf2ab3ca66d293adea7a34115dd96bef32f
Masa Sabobeh <masasabobeh03@gmail.com>
2026-01-24 12:18:39 +0200

    fix: changed the query dto

--- src/modules/pharmacy/dto/pharmacy.dto.ts
+++ src/modules/pharmacy/dto/pharmacy.dto.ts
@@ -1,7 +1,11 @@
 import z from 'zod';
 import { AdminPharmacyListQuerySchema } from '../schema/pharmacy.schema';
+import { UserStatus, VerificationStatus } from '@prisma/client';
 
 //ADMIN: Get pharmacies  query dto
-export type adminPharmacyListQueryDto = z.infer<
-  typeof AdminPharmacyListQuerySchema
->;
+export type adminPharmacyListQueryDto = {
+  status?: VerificationStatus;
+  userStatus?: UserStatus;
+
+  q?: string;
+};
