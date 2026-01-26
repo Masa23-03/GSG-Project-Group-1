7433f96fa1eb28b73302c7496a4bb7e66c1c50cf
Masa Sabobeh <masasabobeh03@gmail.com>
2026-01-26 22:24:49 +0200

    refactor: correct addressWithCity type

--- src/modules/patient-address/types/address.type.ts
+++ src/modules/patient-address/types/address.type.ts
@@ -1,7 +1,16 @@
-import { PatientAddress } from '@prisma/client';
+import { PatientAddress, Prisma } from '@prisma/client';
 
-export type addressWithCity = PatientAddress & {
-  city?: {
-    name: string;
+export type addressWithCity = Prisma.PatientAddressGetPayload<{
+  select: {
+    id: true;
+    label: true;
+    area: true;
+    region: true;
+    cityId: true;
+    addressLine1: true;
+    addressLine2: true;
+    latitude: true;
+    longitude: true;
+    city: { select: { name: true } };
   };
-};
+}>;
