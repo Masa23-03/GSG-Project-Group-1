f4976d9838c1e53b45b2a69104eee68d81c554d4
shahd <eng.shahd.abusharif@gmail.com>
2026-01-28 09:19:46 +0200

    Env Setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { FileModule } from './modules/file/file.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: '.env'
     }),
     AuthModule,
     UserModule,
