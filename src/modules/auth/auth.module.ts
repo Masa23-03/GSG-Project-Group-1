c921316590c5d3891f1bfb4c2d1d9617f3512fa4
Masa Sabobeh <masasabobeh03@gmail.com>
2026-01-28 22:11:22 +0200

    import pharmacy and driver service in auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -5,12 +5,16 @@ import { JwtModule } from '@nestjs/jwt';
 import { env } from 'src/config/env';
 import { OtpService } from './otp/otp.service';
 import { UserModule } from '../user/user.module';
+import { PharmacyModule } from '../pharmacy/pharmacy.module';
+import { DriverModule } from '../driver/driver.module';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService, OtpService],
   imports: [
     UserModule,
+    PharmacyModule,
+    DriverModule,
     JwtModule.registerAsync({
       global: true,
       useFactory: async () => ({
