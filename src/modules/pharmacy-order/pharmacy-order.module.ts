0d80b4aa964bb4c7fedc783b9ff2b84b468a49e8
Masa Sabobeh <masasabobeh03@gmail.com>
2026-01-31 15:11:21 +0200

    refactor: correct it delivery flow with order

--- src/modules/pharmacy-order/pharmacy-order.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PharmacyOrderService } from './pharmacy-order.service';
-import { PharmacyOrderController } from './pharmacy-order.controller';
-
-@Module({
-  controllers: [PharmacyOrderController],
-  providers: [PharmacyOrderService],
-})
-export class PharmacyOrderModule {}
