8e573e021fd8930b21b920a17586101e5f689d94
Masa Sabobeh <masasabobeh03@gmail.com>
2026-02-01 21:43:46 +0200

    exported types

--- src/modules/order/util/types.helper.create.ts
+++ src/modules/order/util/types.helper.create.ts
@@ -1,34 +1,34 @@
 import { Prisma } from '@prisma/client';
 import { OrderService } from '../order.service';
 
-type ReqInv = { pharmacyId: number; quantity: number };
+export type ReqInv = { pharmacyId: number; quantity: number };
 
-type ParsedOrderRequest = {
+export type ParsedOrderRequest = {
   pharmacyIds: number[]; //list of pharmacies ids
   inventoryIds: number[]; //list of all inventory ids
   reqByInventoryId: Map<number, ReqInv>; //inventory it mapped with pharmacy id and quantity
 };
 //to check with them
-type PharmacyLite = {
+export type PharmacyLite = {
   id: number;
   cityId: number;
   workOpenTime: Date | null;
   workCloseTime: Date | null;
 };
 //address snapshot stored on order
-type AddressLite = {
+export type AddressLite = {
   addressLine1: string;
   latitude: Prisma.Decimal | null;
   longitude: Prisma.Decimal | null;
 };
 //user info snapshot stored on order
-type UserContactLite = {
+export type UserContactLite = {
   name: string;
   email: string;
   phoneNumber: string;
 };
 //to check
-type InventoryLite = {
+export type InventoryLite = {
   id: number;
   pharmacyId: number;
   stockQuantity: number;
@@ -36,7 +36,7 @@ type InventoryLite = {
   medicine: { requiresPrescription: boolean };
 };
 //to calculate order total amount + items count
-type PricingResult = {
+export type PricingResult = {
   pharmacyOrdersData: Array<
     ReturnType<OrderService['buildSinglePharmacyOrder']>
   >;
