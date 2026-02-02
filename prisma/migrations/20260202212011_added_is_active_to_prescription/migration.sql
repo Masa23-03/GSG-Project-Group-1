b969d4b4f81ed8235205f24b5ecdbd74cb84758e
Masa Sabobeh <masasabobeh03@gmail.com>
2026-02-02 23:20:34 +0200

    feat: added isActive to prescription table

---
+++ prisma/migrations/20260202212011_added_is_active_to_prescription/migration.sql
@@ -0,0 +1,39 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `last_location_at` on the `drivers` table. The data in that column could be lost. The data in that column will be cast from `Timestamp(0)` to `Timestamp`.
+  - You are about to alter the column `reviewed_at` on the `drivers` table. The data in that column could be lost. The data in that column will be cast from `Timestamp(0)` to `Timestamp`.
+  - You are about to alter the column `verified_at` on the `otps` table. The data in that column could be lost. The data in that column will be cast from `Timestamp(0)` to `Timestamp`.
+  - You are about to alter the column `sent_at` on the `otps` table. The data in that column could be lost. The data in that column will be cast from `Timestamp(0)` to `Timestamp`.
+  - You are about to alter the column `reviewed_at` on the `pharmacies` table. The data in that column could be lost. The data in that column will be cast from `Timestamp(0)` to `Timestamp`.
+  - You are about to alter the column `picked_up_at` on the `pharmacy_orders` table. The data in that column could be lost. The data in that column will be cast from `Timestamp(0)` to `Timestamp`.
+  - You are about to alter the column `accepted_at` on the `pharmacy_orders` table. The data in that column could be lost. The data in that column will be cast from `Timestamp(0)` to `Timestamp`.
+  - You are about to alter the column `rejected_at` on the `pharmacy_orders` table. The data in that column could be lost. The data in that column will be cast from `Timestamp(0)` to `Timestamp`.
+  - You are about to alter the column `reupload_requested_at` on the `prescriptions` table. The data in that column could be lost. The data in that column will be cast from `Timestamp(0)` to `Timestamp`.
+  - You are about to alter the column `expires_at` on the `refresh_tokens` table. The data in that column could be lost. The data in that column will be cast from `Timestamp(0)` to `Timestamp`.
+  - You are about to alter the column `revoked_at` on the `refresh_tokens` table. The data in that column could be lost. The data in that column will be cast from `Timestamp(0)` to `Timestamp`.
+
+*/
+-- AlterTable
+ALTER TABLE `drivers` MODIFY `last_location_at` TIMESTAMP NULL,
+    MODIFY `reviewed_at` TIMESTAMP NULL;
+
+-- AlterTable
+ALTER TABLE `otps` MODIFY `verified_at` TIMESTAMP NULL,
+    MODIFY `sent_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE `pharmacies` MODIFY `reviewed_at` TIMESTAMP NULL;
+
+-- AlterTable
+ALTER TABLE `pharmacy_orders` MODIFY `picked_up_at` TIMESTAMP NULL,
+    MODIFY `accepted_at` TIMESTAMP NULL,
+    MODIFY `rejected_at` TIMESTAMP NULL;
+
+-- AlterTable
+ALTER TABLE `prescriptions` ADD COLUMN `isActive` BOOLEAN NOT NULL DEFAULT true,
+    MODIFY `reupload_requested_at` TIMESTAMP NULL;
+
+-- AlterTable
+ALTER TABLE `refresh_tokens` MODIFY `expires_at` TIMESTAMP NOT NULL,
+    MODIFY `revoked_at` TIMESTAMP NULL;
