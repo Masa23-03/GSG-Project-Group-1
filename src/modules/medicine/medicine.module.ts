dc9f86de7a79aea184f496c6dfbf6091b4b43018
shahd <eng.shahd.abusharif@gmail.com>
2026-02-05 20:08:57 +0200

    update MedicineModule

--- src/modules/medicine/medicine.module.ts
+++ src/modules/medicine/medicine.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { MedicineService } from './medicine.service';
+import { DatabaseModule } from '../database/database.module';
 import { MedicineController } from './medicine.controller';
+import { PharmacyModule } from '../pharmacy/pharmacy.module';
+import { MedicineAdminService } from './medicine.admin.service';
+import { MedicinePharmacyService } from './medicine.pharmacy.service';
+import { MedicineAdminController } from './medicine.admin.controller';
+import { MedicinePharmacyController } from './medicine.pharmacy.controller';
+
 
 @Module({
-  controllers: [MedicineController],
-  providers: [MedicineService],
+  controllers: [
+    MedicineAdminController,
+    MedicinePharmacyController,
+    MedicineController,
+  ],  imports: [DatabaseModule , PharmacyModule],
+  providers: [MedicineService, MedicineAdminService, MedicinePharmacyService],
+  exports: [MedicineService], 
 })
-export class MedicineModule {}
+export class MedicineModule { }
