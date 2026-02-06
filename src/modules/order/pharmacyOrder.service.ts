fa92f019d08cfe3aab1e277170a0d886b6524597
Masa Sabobeh <masasabobeh03@gmail.com>
2026-02-06 04:44:11 +0200

    feat: add get pharmacy orders to pharmacy order service

---
+++ src/modules/order/pharmacyOrder.service.ts
@@ -0,0 +1,53 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { DatabaseService } from '../database/database.service';
+import { PharmacyOrderQueryDto } from './dto/request.dto/order.query.dto';
+import { PharmacyOrderListResponseDto } from './dto/response.dto/pharmacyOrder.response.dto';
+import { buildPharmacyOrderWhereStatement } from './util/pharmacyOrderWhereBuilder.util';
+import { buildCreatedAtOrderBy } from 'src/types/pagination.query';
+import { removeFields } from 'src/utils/object.util';
+import {
+  mapToPharmacyOrderItemList,
+  mapToPharmacyOrderList,
+  pharmacyOrderWithRelations,
+} from './util/pharmacyOrder.mapper';
+import { ApiPaginationSuccessResponse } from 'src/types/unifiedType.types';
+
+@Injectable()
+export class PharmacyOrderService {
+  constructor(private readonly prismaService: DatabaseService) {}
+
+  //! get orders list
+  async getOrders(
+    pharmacyId: number,
+    query: PharmacyOrderQueryDto,
+  ): Promise<ApiPaginationSuccessResponse<PharmacyOrderListResponseDto>> {
+    const pagination = this.prismaService.handleQueryPagination(query);
+    const orderBy = buildCreatedAtOrderBy(query);
+    const whereSt = buildPharmacyOrderWhereStatement(pharmacyId, query);
+
+    const [orders, count] = await Promise.all([
+      this.prismaService.pharmacyOrder.findMany({
+        ...removeFields(pagination, ['page']),
+        where: whereSt,
+        orderBy: orderBy,
+
+        include: pharmacyOrderWithRelations,
+      }),
+      this.prismaService.pharmacyOrder.count({
+        where: whereSt,
+      }),
+    ]);
+    const data: PharmacyOrderListResponseDto[] = orders.map(
+      mapToPharmacyOrderList,
+    );
+    return {
+      success: true,
+      data: data,
+      meta: this.prismaService.formatPaginationResponse({
+        count,
+        page: pagination.page,
+        limit: pagination.take,
+      }),
+    };
+  }
+}
