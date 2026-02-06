3ef95b0721ba263b4c50dd9ebbe4f79cf113fea0
Masa Sabobeh <masasabobeh03@gmail.com>
2026-02-06 10:37:10 +0200

    feat: add pharmacy order decision body dto

--- src/modules/order/dto/request.dto/update-order.dto.ts
+++ src/modules/order/dto/request.dto/update-order.dto.ts
@@ -1,4 +1,20 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional, PartialType } from '@nestjs/swagger';
 import { CreateOrderDto } from './create-order.dto';
 
 export class UpdateOrderDto extends PartialType(CreateOrderDto) {}
+
+export enum PharmacyOrderDecision {
+  ACCEPT = 'ACCEPT',
+  REJECT = 'REJECT',
+}
+
+export class PharmacyOrderDecisionDto {
+  @ApiProperty({ enum: PharmacyOrderDecision })
+  decision!: PharmacyOrderDecision;
+
+  @ApiPropertyOptional({
+    description: 'Required when decision is REJECT',
+    nullable: true,
+  })
+  rejectionReason?: string | null;
+}
