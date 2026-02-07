d998a33df7a31e522fb8f7f6e957c82ee74f090a
Masa Sabobeh <masasabobeh03@gmail.com>
2026-02-08 00:34:28 +0200

    feat: create prescription dto

--- src/modules/prescription/dto/create-prescription.dto.ts
+++ src/modules/prescription/dto/create-prescription.dto.ts
@@ -1,1 +1,34 @@
-export class CreatePrescriptionDto {}
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
+export class CreatePrescriptionFileDto {
+  @ApiProperty({
+    description: 'Public URL of the uploaded prescription file.',
+  })
+  url!: string;
+  @ApiPropertyOptional({
+    description: 'Optional display order for this file.',
+    example: 1,
+  })
+  sortOrder?: number;
+}
+export class CreatePrescriptionDto {
+  @ApiProperty({
+    description: 'Pharmacy ID that this prescription belongs to.',
+    example: 12,
+  })
+  pharmacyId!: number;
+  @ApiProperty({
+    description:
+      'List of uploaded prescription files (URLs). Must include at least one file.',
+    type: [CreatePrescriptionFileDto],
+  })
+  files!: CreatePrescriptionFileDto[];
+}
+export class ReuploadPrescriptionDto {
+  @ApiProperty({
+    description:
+      'New set of prescription files (URLs) for re-upload. Must include at least one file.',
+    type: [CreatePrescriptionFileDto],
+  })
+  files!: CreatePrescriptionFileDto[];
+}
