4c85feda4604d14af20c94f9985f9718d71b3042
Masa Sabobeh <masasabobeh03@gmail.com>
2026-02-07 10:31:04 +0200

    add pharmacy order controller to order module

--- src/modules/order/order.module.ts
+++ src/modules/order/order.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
 import { PharmacyOrderService } from './pharmacyOrder.service';
+import { PharmacyOrderController } from './pharmacyOrder.controller';
 
 @Module({
-  controllers: [OrderController],
+  controllers: [OrderController, PharmacyOrderController],
   providers: [OrderService, PharmacyOrderService],
 })
 export class OrderModule {}
