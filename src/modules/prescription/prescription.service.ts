0abd6716d9a54b237ebfe9b4cb41e46933c63635
Masa Sabobeh <masasabobeh03@gmail.com>
2026-02-08 02:35:46 +0200

    create prescription method

--- src/modules/prescription/prescription.service.ts
+++ src/modules/prescription/prescription.service.ts
@@ -1,14 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreatePrescriptionDto } from './dto/create-prescription.dto';
+import { PrescriptionResponseDto } from './dto/response/response.dto';
+import { DatabaseService } from '../database/database.service';
+import {
+  PrescriptionStatus,
+  UserStatus,
+  VerificationStatus,
+} from '@prisma/client';
+import { mapToPrescriptionResponse } from './util/prescription.mapper';
 
 @Injectable()
 export class PrescriptionService {
-  create(createPrescriptionDto: CreatePrescriptionDto) {
-    return 'This action adds a new prescription';
-  }
+  constructor(private readonly prismaService: DatabaseService) {}
+  async create(
+    userId: number,
+    dto: CreatePrescriptionDto,
+  ): Promise<PrescriptionResponseDto> {
+    return this.prismaService.$transaction(async (prisma) => {
+      const pharmacy = await prisma.pharmacy.findFirst({
+        where: {
+          id: dto.pharmacyId,
+          verificationStatus: VerificationStatus.VERIFIED,
+          user: { status: UserStatus.ACTIVE },
+        },
+        select: { id: true },
+      });
+      if (!pharmacy) throw new NotFoundException('Pharmacy not found');
+      const prescription = await prisma.prescription.create({
+        data: {
+          patientId: userId,
+          pharmacyId: pharmacy.id,
+          isActive: true,
+          status: PrescriptionStatus.UNDER_REVIEW,
+          prescriptionFiles: {
+            create: dto.fileUrls.map((url, index) => ({
+              url: url,
+              sortOrder: index + 1,
+            })),
+          },
+        },
+        include: {
+          prescriptionFiles: true,
+        },
+      });
 
-  findAll() {
-    return `This action returns all prescription`;
+      return mapToPrescriptionResponse(prescription);
+    });
   }
 
   findOne(id: number) {
@@ -17,9 +58,5 @@ export class PrescriptionService {
 
   update(id: number, updatePrescriptionDto) {
     return `This action updates a #${id} prescription`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} prescription`;
   }
 }
