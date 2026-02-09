d00391cfbb32ec2c7800768160c29e8507040358
Masa Sabobeh <masasabobeh03@gmail.com>
2026-02-09 13:35:31 +0200

    add patient pharmacy details mapper

--- src/modules/pharmacy/util/mapper.ts
+++ src/modules/pharmacy/util/mapper.ts
@@ -1,7 +1,10 @@
 import { Prisma } from '@prisma/client';
 import { PatientPharmaciesQueryDto } from '../dto/query.dto/patient.query.dto';
-import { PatientPharmacyListResponseDto } from '../dto/response.dto/pateint-pharmacy.response.dto';
-import { calculateDistanceKMOrNull, isPharmacyOpenNow } from './helper';
+import {
+  PatientPharmacyDetailsDto,
+  PatientPharmacyListResponseDto,
+} from '../dto/response.dto/pateint-pharmacy.response.dto';
+import { calculateDistanceKMOrNull, isPharmacyOpenNow, toHHmm } from './helper';
 
 export const patientPharmacySelect = {
   id: true,
@@ -27,9 +30,16 @@ export const patientPharmacySelect = {
 
   user: { select: { profileImageUrl: true, status: true } },
 } satisfies Prisma.PharmacySelect;
-
+export const patientPharmacyDetailsSelect = {
+  ...patientPharmacySelect,
+  user: { select: { profileImageUrl: true, status: true, phoneNumber: true } },
+};
 export type PatientPharmacyWithRelations = Prisma.PharmacyGetPayload<{
   select: typeof patientPharmacySelect;
+}>;
+
+export type PatientPharmacyDetailsWithRelations = Prisma.PharmacyGetPayload<{
+  select: typeof patientPharmacyDetailsSelect;
 }>;
 
 export function mapToPatientPharmacyList(
@@ -69,5 +79,49 @@ export function mapToPatientPharmacyList(
     profileImageUrl: pharmacy.user.profileImageUrl ?? null,
 
     isOpenNow: isPharmacyOpenNow(pharmacy.workOpenTime, pharmacy.workCloseTime),
+  };
+}
+export function mapToPatientPharmacyDetails(
+  pharmacy: PatientPharmacyDetailsWithRelations,
+  patientLat?: number,
+  patientLng?: number,
+): PatientPharmacyDetailsDto {
+  const distanceKm = calculateDistanceKMOrNull(
+    patientLat,
+    patientLng,
+    pharmacy.latitude !== null ? Number(pharmacy.latitude) : undefined,
+    pharmacy.longitude !== null ? Number(pharmacy.longitude) : undefined,
+  );
+
+  const deliveryFee = pharmacy.city.cityDeliveryFee
+    ? Number(pharmacy.city.cityDeliveryFee.standardFeeAmount)
+    : null;
+
+  return {
+    id: pharmacy.id,
+    pharmacyName: pharmacy.pharmacyName,
+
+    cityId: pharmacy.cityId,
+    cityName: pharmacy.city.name,
+
+    address: {
+      address: pharmacy.address ?? null,
+      latitude: pharmacy.latitude !== null ? Number(pharmacy.latitude) : null,
+      longitude:
+        pharmacy.longitude !== null ? Number(pharmacy.longitude) : null,
+    },
+
+    distanceKm,
+    eta: null,
+    deliveryFee,
+
+    coverImageUrl: pharmacy.coverImageUrl ?? null,
+    profileImageUrl: pharmacy.user.profileImageUrl ?? null,
+
+    isOpenNow: isPharmacyOpenNow(pharmacy.workOpenTime, pharmacy.workCloseTime),
+
+    workOpenTime: toHHmm(pharmacy.workOpenTime),
+    workCloseTime: toHHmm(pharmacy.workCloseTime),
+    phoneNumber: pharmacy.user.phoneNumber,
   };
 }
