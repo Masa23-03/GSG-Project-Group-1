4d940f019ccf9811d1a6421dc0b4f2a0caac60b1
Masa Sabobeh <masasabobeh03@gmail.com>
2026-02-10 16:35:58 +0200

    make pagination dto better

--- src/types/pagination.query.ts
+++ src/types/pagination.query.ts
@@ -1,9 +1,20 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class PaginationQueryDto {
-  @ApiPropertyOptional({ default: 1 })
+  @ApiPropertyOptional({
+    example: 1,
+    default: 1,
+    minimum: 1,
+    description: 'Page number (starting from 1).',
+  })
   page?: number;
 
-  @ApiPropertyOptional({ default: 10, maximum: 100 })
+  @ApiPropertyOptional({
+    example: 10,
+    default: 10,
+    minimum: 1,
+    maximum: 100,
+    description: 'Number of items per page (max 100).',
+  })
   limit?: number;
 }
