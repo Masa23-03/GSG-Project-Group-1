4e458a727bec985fd6af57c700af9b7c8ada18a8
Masa Sabobeh <masasabobeh03@gmail.com>
2026-02-10 19:10:41 +0200

    get available deliveries method

--- src/modules/deliveries/deliveries.service.ts
+++ src/modules/deliveries/deliveries.service.ts
@@ -1,13 +1,78 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { ApiPaginationSuccessResponse } from 'src/types/unifiedType.types';
+import { DriverAvailableDeliveriesListItemDto } from './dto/response/list.response.dto';
+import { DatabaseService } from '../database/database.service';
+import {
+  AvailabilityStatus,
+  DeliveryStatus,
+  PharmacyOrderStatus,
+} from '@prisma/client';
+import { removeFields } from 'src/utils/object.util';
+import {
+  deliveryAvailableListSelect,
+  mapDeliveryListItem,
+} from './util/list.mapper';
+import { DriverDeliveriesListQueryDto } from './dto/request/query.dto';
+import { SortOrder } from '../order/dto/request.dto/order.query.dto';
 
 @Injectable()
 export class DeliveriesService {
-  create(createDeliveryDto) {
-    return 'This action adds a new delivery';
-  }
+  constructor(private readonly prismaService: DatabaseService) {}
+  async getAvailableDeliveries(
+    userId: number,
+    query: DriverDeliveriesListQueryDto,
+  ): Promise<
+    ApiPaginationSuccessResponse<DriverAvailableDeliveriesListItemDto>
+  > {
+    const driver = await this.prismaService.driver.findUnique({
+      where: {
+        userId,
+      },
+      select: {
+        availabilityStatus: true,
+      },
+    });
+    if (!driver) throw new NotFoundException('Driver not found');
+    if (driver.availabilityStatus !== AvailabilityStatus.ONLINE)
+      throw new ForbiddenException('Driver must be ONLINE');
+    const pagination = await this.prismaService.handleQueryPagination(query);
+    const where = {
+      driverId: null,
+      status: DeliveryStatus.PENDING,
+      pharmacyOrders: {
+        every: {
+          status: PharmacyOrderStatus.READY_FOR_PICKUP,
+        },
+      },
+    };
+    const [rows, count] = await Promise.all([
+      this.prismaService.delivery.findMany({
+        ...removeFields(pagination, ['page']),
+        where,
+        select: deliveryAvailableListSelect,
+        orderBy: [
+          { createdAt: query.sortOrder ?? SortOrder.DESC },
+          { id: 'desc' },
+        ],
+      }),
+      this.prismaService.delivery.count({
+        where,
+      }),
+    ]);
 
-  getAvailableDeliveries() {
-    return `This action returns all deliveries`;
+    return {
+      success: true,
+      data: rows.map((d) => mapDeliveryListItem(d)),
+      meta: await this.prismaService.formatPaginationResponse({
+        count,
+        page: pagination.page,
+        limit: pagination.take,
+      }),
+    };
   }
 
   findOne(id: number) {
@@ -16,9 +81,5 @@ export class DeliveriesService {
 
   update(id: number, updateDeliveryDto) {
     return `This action updates a #${id} delivery`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} delivery`;
   }
 }
