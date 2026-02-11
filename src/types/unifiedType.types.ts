58d5811aaac350687cc2b5d69a9c7cb10f405d30
shahd <eng.shahd.abusharif@gmail.com>
2026-02-11 23:16:12 +0200

    moving Params type into unifiedtypes.ts

--- src/types/unifiedType.types.ts
+++ src/types/unifiedType.types.ts
@@ -1,11 +1,22 @@
 import { HttpStatus } from '@nestjs/common';
-import { UserRole } from '@prisma/client';
+import { MedicineStatus, UserRole } from '@prisma/client';
 
 export type objectType = Record<string, unknown>;
 export type authedUserType = {
   id: number;
   role: UserRole;
 };
+
+
+export type Params = {
+  q?: string
+  status?: MedicineStatus
+  isActive?: boolean
+  categoryId?: number
+  page: number
+  limit: number
+}
+
 export type PaginationQueryType = {
   page?: number;
   limit?: number;
