0f98d51a5847faa0e402a8d7bcaa2b4f110fc346
Masa Sabobeh <masasabobeh03@gmail.com>
2026-02-13 00:29:18 +0200

    stage decorater removed

--- src/types/declaration-merging.ts
+++ src/types/declaration-merging.ts
@@ -1,13 +1,11 @@
 import { UserRole } from '@prisma/client';
-import { AuthStage } from 'src/decorators/stage.decorator';
 
 declare global {
   namespace Express {
     interface Request {
-      user?: { 
-        id: number; 
-        role: UserRole; 
-        stage?: AuthStage 
+      user?: {
+        id: number;
+        role: UserRole;
       };
     }
   }
