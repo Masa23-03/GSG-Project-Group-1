7313f0b7981f61d7d459fd6722c117303f4c75a8
Masa Sabobeh <masasabobeh03@gmail.com>
2026-02-14 15:46:46 +0200

    changed password max into 128

--- src/utils/zod.helper.ts
+++ src/utils/zod.helper.ts
@@ -15,7 +15,7 @@ export const phoneSchema = z
 export const passwordSchema = z
   .string()
   .min(8, 'Password must be at least 8 characters long')
-  .max(100, 'Password is too long')
+  .max(128, 'Password is too long')
   .regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
   .regex(/[0-9]/, 'Password must contain at least one number')
   .regex(/[^A-Za-z0-9]/, 'Password must contain at least one special character')
