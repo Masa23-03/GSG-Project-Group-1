ac89f7f540de0863e2b9be062f632ad94699c2d5
Masa Sabobeh <masasabobeh03@gmail.com>
2026-02-24 02:03:44 +0200

    add category to medicine include

--- src/modules/medicine/util/medicine.shared.ts
+++ src/modules/medicine/util/medicine.shared.ts
@@ -1,10 +1,11 @@
 import { Prisma } from '@prisma/client';
 
-export const medicineInclude: Prisma.MedicineInclude = {
+export const medicineInclude = {
   medicineImages: {
     orderBy: { sortOrder: 'asc' },
   },
-};
+  category: { select: { id: true, name: true } },
+} satisfies Prisma.MedicineInclude;
 
 export type MedicineWithImages = Prisma.MedicineGetPayload<{
   include: typeof medicineInclude;
