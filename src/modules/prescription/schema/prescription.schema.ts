7c5ce3e42ec7ed2285432dc259dbeb3c9a982f87
Masa Sabobeh <masasabobeh03@gmail.com>
2026-02-26 21:38:51 +0200

    disallow duplicated urls

--- src/modules/prescription/schema/prescription.schema.ts
+++ src/modules/prescription/schema/prescription.schema.ts
@@ -1,16 +1,26 @@
 import { safeText, urlSchema } from 'src/utils/zod.helper';
 import z from 'zod';
 
+const uniqueUrls = (arr: string[]) => new Set(arr).size === arr.length;
+
 export const createPrescriptionSchema = z
   .object({
     pharmacyId: z.number().int().min(1),
-    fileUrls: z.array(urlSchema).min(1).max(5),
+    fileUrls: z
+      .array(urlSchema.transform((u) => u.trim()))
+      .min(1)
+      .max(5)
+      .refine(uniqueUrls, { message: 'Duplicate file URLs are not allowed' }),
   })
   .strict();
 
 export const reuploadPrescriptionSchema = z
   .object({
-    fileUrls: z.array(urlSchema).min(1).max(5),
+    fileUrls: z
+      .array(urlSchema.transform((u) => u.trim()))
+      .min(1)
+      .max(5)
+      .refine(uniqueUrls, { message: 'Duplicate file URLs are not allowed' }),
   })
   .strict();
 
