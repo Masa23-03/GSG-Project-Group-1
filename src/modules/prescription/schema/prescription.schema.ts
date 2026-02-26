d7784850da0575dfef0836354e5b894ec17dd686
Masa Sabobeh <masasabobeh03@gmail.com>
2026-02-26 08:14:15 +0200

    use safeText and urlSchema  in prescription schema

--- src/modules/prescription/schema/prescription.schema.ts
+++ src/modules/prescription/schema/prescription.schema.ts
@@ -1,18 +1,21 @@
+import { safeText, urlSchema } from 'src/utils/zod.helper';
 import z from 'zod';
 
 export const createPrescriptionSchema = z
   .object({
     pharmacyId: z.number().int().min(1),
-    fileUrls: z.array(z.string().trim().min(5)).min(1),
+    fileUrls: z.array(urlSchema).min(1).max(5),
   })
   .strict();
 
 export const reuploadPrescriptionSchema = z
   .object({
-    fileUrls: z.array(z.string().trim().min(5)).min(1),
+    fileUrls: z.array(urlSchema).min(1).max(5),
   })
   .strict();
 
-export const requestNewPrescriptionSchema = z.object({
-  reuploadReason: z.string().trim().min(1),
-});
+export const requestNewPrescriptionSchema = z
+  .object({
+    reuploadReason: safeText({ min: 1, max: 500, mode: 'generic' }),
+  })
+  .strict();
